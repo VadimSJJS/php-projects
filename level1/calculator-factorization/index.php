2d928dfc5b1991105834526f188ef3f6c6ee3eca
VadimSJJS <vadimvaluev72@gmail.com>
2025-11-04 16:38:11 +0300

    test commit

---
+++ level1/calculator-factorization/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Разложение на простые множители</title>
+</head>
+<header>
+    <h1>Проект "Разложение на простые множители"</h1>
+</header>
+
+<div class="description">
+    <p>Проект представляет собой калькулятор, разлагающий числа на простые множители. </p>
+
+    <div class="condition">
+        <h2>Описание</h2>
+        <p class="">Калькулятор должен представлять собой инпут, в который вводится число, и кнопку, по нажатию на которую наш сайт разлагает это число на простые множители. </p>
+    </div>
+    
+</div>
+
+<body>
+
+</body>
+
+</html>
