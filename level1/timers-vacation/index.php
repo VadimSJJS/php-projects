16e45c6c9331b70adbe03a2781b93d4a6cff782e
VadimSJJS <vadimvaluev72@gmail.com>
2025-11-11 14:09:38 +0300

    ready timers-vacation project

--- level1/timers-vacation/index.php
+++ level1/timers-vacation/index.php
@@ -1,33 +1,65 @@
 <?php
-$currentDate = date('d-m-Y');
+session_start();
 
-// дата в формате: x ноября x года
-function getDateStrFormat() {
-    $arr = [
-        1 => 'Январь',
-        2 => 'Февраль',
-        3 => 'Март',
-        4 => 'Апрель',
-        5 => 'Май',
-        6 => 'Июнь',
-        7 => 'Июль',
-        8 => 'Август',
-        9 => 'Сентябрь',
-        10 => 'Октябрь',
-        11 => 'Ноябрь',
-        12 => 'Декабрь',
-    ];
+// Текущая дата
+$currentDate = date('d.m.Y H:i:s');
+$errorDate = "";
+
+function showVacationUntil()
+{
+    global $currentDate, $vacationDate;
     
+    $diff = strtotime($vacationDate) - strtotime($currentDate);
+    
+    $days = floor($diff / (60 * 60 * 24));
+    $hours = floor(($diff % (60 * 60 * 24)) / (60 * 60));
+    $minutes = floor(($diff % (60 * 60)) / 60);
+
+    echo "<div class=\"untilVacation\">";
+    echo "<p>Текущая дата: " . $currentDate . "</p>";
+    echo "<p>Дата отпуска: " . $vacationDate . "</p>";
+    // Если отпуск настал
+    if ($days == 0 && $hours == 0 && $minutes == 0) {
+        echo "<p>Отпуск наступил, хорошо провести отпуск!</p>";
+    } else {
+        echo "<p>Осталось до отпуска: "  . $days . " дней, " . $hours . " часов, " . $minutes . " минут" . " </p>";
+    }
+    echo "</div>";
 }
+
+// проверка чтобы дата отспуска была больше чем текущая дата
+function isVacationDateValid($currentDate, $vacationDate)
+{
+    $currentTimestamp = strtotime(str_replace('.', '-', $currentDate));
+    $vacationTimestamp = strtotime($vacationDate);
+    return $vacationTimestamp >= $currentTimestamp;
+}
+
+// Дата отпуска
+if (isset($_POST['calculateUntilVacation'])) {
+    if (empty($_POST['vacationDate'])) {
+        $errorDate = "Вы не указали дату";
+    } else {
+        $vacationDate = date("d.n.Y H:i:s", strtotime($_POST['vacationDate']));
+        if (!isVacationDateValid($currentDate, $vacationDate)) {
+            $errorDate = "Вы указали дату отпуска: " . $vacationDate . ". Но нужно указывать дату отпуска которая не меньше текущей даты";
+        } else {
+            $_SESSION['vacationDate'] = $vacationDate;
+            $errorDate = "";
+        }
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Таймер до отпуска</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -40,25 +72,37 @@ function getDateStrFormat() {
 
         <div class="description-block">
             <h2>Установка времени</h2>
-
             <p>При заходе на сайт должна появиться форма, с помощью которой пользователь должен ввести дату начала отпуска. Сайт должен запомнить эту информацию, а также дать возможность ее отредактировать.</p>
         </div>
 
         <div class="description-block">
             <h2>Работа таймера</h2>
-
             <p>Таймер должен вести обратный отсчет дней, часов и минут, оставшихся до отпуска. Когда таймер истечет, сайт должен пожелать пользователю хорошо провести отпуск.</p>
         </div>
     </div>
 
     <div class="main">
-        <p>Текущая дата: <?= $currentDate ?></p>
-        <input type="button" value="Поменять формат на дд.мм.гггг" >
-        <p>Укажите дату отпуска</p>
-        <form action="" method="GET">
-            <input type="date">
+        <div class="current-date">
+            Текущая дата: <?= $currentDate ?>
+        </div>
+
+        <form action="" method="POST">
+            <div>
+                <p>Укажите дату отпуска</p>
+                <input type="date" name="vacationDate">
+                <?= $errorDate; ?>
+            </div>
+
+            <button type="submit" name="calculateUntilVacation" class="format-btn">
+                Посчитать сколько осталось до отпуска
+            </button>
+
+            <?php 
+                if (empty($errorDate)) {
+                    showVacationUntil();
+                }
+            ?>
         </form>
     </div>
 </body>
-
 </html>
